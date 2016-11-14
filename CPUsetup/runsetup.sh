9790f762768a2aad8e1e32c0bef19200180147cc
cescalara <capel.francesca@gmail.com>
2016-11-14 17:15:50 +0100

    Updated the runsetup.sh file

--- CPUsetup/runsetup.sh
+++ CPUsetup/runsetup.sh
@@ -7,6 +7,7 @@
 
 ZYNQ_IP=192.168.7.10
 CPU_IP=192.168.7.2
+TEST_IP=8.8.8.8
 
 echo "Mini-EUSO CPU setup"
 echo "*******************"
@@ -16,50 +17,48 @@ if ping -q -c 1 -W 1 $TEST_IP > /dev/null 2>&1
 then
    echo "Connected to internet..."
    echo "Downloading packages..."
-   apt-get update && upgrade > /dev/null 2>&1
-   apt-get install build-essential vsftpd expect > /dev/null 2>&1
+   #apt-get update && upgrade > /dev/null 2>&1
+   #apt-get install build-essential vsftpd expect > /dev/null 2>&1
 else
-       echo "Could not connect to internet. Exiting.."
+       echo "Could not connect to internet. Exiting..."
        exit 1
 fi
 
 #Setup the FTP server
 echo "Setting up the FTP server..."
-mkdir /home/ftp-home > /dev/null 2>&1
-./create_ftpuser.sh
-sleep 10
+mkdir /home/minieusouser/DATA > /dev/null 2>&1
+chown minieusouser /home/minieusouser/DATA
+mkdir /home/minieusouser/DONE > /dev/null 2>&1
+chown minieusouser /home/minieusouser/DONE 
+rm /etc/vsftpd.conf > /dev/null 2>&1
+cp /home/minieusouser/CPU/CPUsetup/vsftpd.conf /etc/ > /dev/null 2>&1
 
 #Setup symlinks for commands
 echo "Creating symlinks"
-mv -r /home/CPU/zynq/scripts /home/ftp-home/
-ln -s /home/ftp-home/scripts/acqstart_telnet.sh /usr/local/bin/acqstart_telnet
-ln -s /home/ftp-home/scripts/cpu_poll.sh /usr/local/bin/cpu_poll
-ln -s /home/ftp-home/scripts/send_telnet_cmd.sh /usr/local/bin/send_telnet_cmd
+ln -s /home/minieusouser/CPU/zynq/scripts/acqstart_telnet.sh /usr/local/bin/acqstart_telnet
+ln -s /home/minieusouser/CPU/zynq/scripts/cpu_poll.sh /usr/local/bin/cpu_poll
+ln -s /home/minieusouser/zynq/scripts/send_telnet_cmd.sh /usr/local/bin/send_telnet_cmd
 
 #Network configuration
 echo "Setting up the network configuration..."
-echo "/n" > /etc/network/interfaces
-echo "auto eth0" > /etc/network/interfaces
-echo "iface eth0 inet static" > /etc/network/interfaces
-echo "\taddress 192.168.7.2" > /etc/network/interfaces
-echo "\tnetmask 255.255.255.0" > /etc/network/interfaces
-echo "\tgateway 192.168.7.254" > /etc/network/interfaces
+echo "auto eth0" >> /etc/network/interfaces
+echo "iface eth0 inet static" >> /etc/network/interfaces
+echo "	address 192.168.7.2" >> /etc/network/interfaces
+echo "	netmask 255.255.255.0" >> /etc/network/interfaces
+echo "	gateway 192.168.7.254" >> /etc/network/interfaces
 
 #Setup autologin to root 
 echo "Setting up autologin to root user on boot..."
-systemctl set-default multi-user.target
 mkdir /etc/systemd/system/getty@tty1.service.d
-touch /etc/systemd/getty@tty1.service.d/autologin.conf
-echo "[Service]" > /etc/systemd/getty@tty1.service.d/autologin.conf
-echo "ExecSart=" > /etc/systemd/getty@tty1.service.d/autologin.conf
-echo "ExecStart=-/sbin/agety -a root --noclear %I $TERM" > /etc/systemd/getty@tty1.service.d/autologin.conf
+touch /etc/systemd/system/getty@tty1.service.d/autologin.conf
+> /etc/systemd/system/getty@tty1.service.d/autologin.conf
+echo "[Service]" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
+echo "ExecStart=" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
+echo "ExecStart=-/sbin/agetty -a root --noclear %I $TERM" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
 systemctl daemon-reload
-systemctl restart getty@tty1.service
 
-#Reboot
-echo "Rebooting..."
-sleep 3
-reboot
+#Restart the terminal
+systemctl restart getty@tty1.service
 
 
 

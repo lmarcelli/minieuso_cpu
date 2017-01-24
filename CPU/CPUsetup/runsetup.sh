a9df9fe206ff1b3900a464e130d6959624718c48
cescalara <capel.francesca@gmail.com>
2017-01-24 15:55:41 +0900

    Updated CPUsetup/runsetup.sh

--- CPU/CPUsetup/runsetup.sh
+++ CPU/CPUsetup/runsetup.sh
@@ -19,7 +19,9 @@ then
    echo "Downloading packages..."
    apt-get -y update > /dev/null 2>&1
    apt-get -y install build-essential vsftpd expect libraw1394-11 libgtk2.0-0 \
-   libgtkmm-2.4-dev libglademm-2.4-dev libgtkglextmm-x11-1.2-dev libusb-1.0-0 > /dev/null 2>&1
+   libgtkmm-2.4-dev libglademm-2.4-dev libgtkglextmm-x11-1.2-dev libusb-1.0-0 \
+   stress > /dev/null 2>&1
+   echo "OK"
 else
        echo "Could not connect to internet. Exiting..."
        exit 1
@@ -34,6 +36,7 @@ chown minieusouser /home/minieusouser/DONE
 rm /etc/vsftpd.conf > /dev/null 2>&1
 cp /home/minieusouser/CPU/CPUsetup/vsftpd.conf /etc/ > /dev/null 2>&1
 mkdir /media/usb > /dev/null 2>&1
+echo "OK"
 
 #Setup symlinks for commands
 echo "Creating symlinks"
@@ -41,15 +44,38 @@ ln -s /home/minieusouser/CPU/zynq/scripts/acqstart_telnet.sh /usr/local/bin/acqs
 ln -s /home/minieusouser/CPU/zynq/scripts/cpu_poll.sh /usr/local/bin/cpu_poll
 ln -s /home/minieusouser/zynq/scripts/send_telnet_cmd.sh /usr/local/bin/send_telnet_cmd
 ln -s /home/minieusouser/test/bin/test_systems /usr/local/bin/test_systems
+echo "OK"
 
-#Network configuration
+#Network configuration (need to comment the previous setup)
 echo "Setting up the network configuration..."
-echo "	" >> /etc/network/interfaces
-echo "auto eth1" >> /etc/network/interfaces
-echo "iface eth1 inet static" >> /etc/network/interfaces
-echo "	address 192.168.7.2" >> /etc/network/interfaces
-echo "	netmask 255.255.255.0" >> /etc/network/interfaces
-echo "	gateway 192.168.7.254" >> /etc/network/interfaces
+cp /home/minieusouser/CPU/CPUsetup/interfaces /etc/network/ > /dev/null 2>&1
+echo "OK"
+
+#Setup the cameras 
+echo "Setting up the camera software..."
+sh /home/minieusouser/CPU/cameras/flycapture2-2.3.2.14-i386/install_flycapture.sh
+sh -c 'echo 1000 > /sys/module/usbcore/parameters/usbfs_memory_mb'
+echo "cat /sys/module/usbcore/parameters/usbfs_memory_mb: "
+cat /sys/module/usbcore/parameters/usbfs_memory_mb
+echo "OK"
+
+#Setup the analog board
+echo "Setting up the analog board software..."
+touch /etc/modprobe.d/blacklist.conf
+> /etc/modprobe.d/blacklist.conf 
+echo "blacklist rtd520" >> /etc/modprobe.d/blacklist.conf
+echo "rtd_dm75xx" >> /etc/modules
+make -C /home/minieusouser/CPU/analog/driver
+(cd /home/minieusouser/CPU/analog/driver && make load)
+cp /home/minieusouser/CPU/analog/driver/rtd-dm75xx.ko /lib/modules/3.16.0-4-686-pae/kernel/rtd/
+(cd /home/minieusouser/CPU/analog/driver && depmod -a)
+echo "lsmod | grep rtd:"
+lsmod | grep rtd
+echo "OK"
+
+
+#Press enter to continue
+read -p "Press enter to continue"
 
 #Setup autologin to root 
 echo "Setting up autologin to root user on boot..."
@@ -60,8 +86,7 @@ echo "[Service]" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
 echo "ExecStart=" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
 echo "ExecStart=-/sbin/agetty -a root --noclear %I $TERM" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
 systemctl daemon-reload
-
-sleep 30
+echo "OK"
 
 #Restart the terminal
 systemctl restart getty@tty1.service

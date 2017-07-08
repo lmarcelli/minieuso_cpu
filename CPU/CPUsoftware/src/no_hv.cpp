918ba2792919dc95071f9c83e858facba54d2a16
Francesca Capel <capel.francesca@gmail.com>
2017-07-08 12:25:45 +0200

    Update to skip S-curve acquisition with new firmware

--- CPU/CPUsoftware/src/no_hv.cpp
+++ CPU/CPUsoftware/src/no_hv.cpp
@@ -55,11 +55,11 @@ int main(void) {
   //HvpsTurnOn(ConfigOut.cathode_voltage, ConfigOut.dynode_voltage);
 
   /* take an scurve */
-  std::thread check_sc (ProcessIncomingData, current_run_file, ConfigOut);
+  //std::thread check_sc (ProcessIncomingData, current_run_file, ConfigOut);
 
-  Scurve(ConfigOut->scurve_start, ConfigOut->scurve_step, ConfigOut->scurve_stop, ConfigOut->scurve_acc);
+  //Scurve(ConfigOut->scurve_start, ConfigOut->scurve_step, ConfigOut->scurve_stop, ConfigOut->scurve_acc);
 
-  check_sc.join();
+  //check_sc.join();
   
   /* set the DAC level */
   SetDac(750);

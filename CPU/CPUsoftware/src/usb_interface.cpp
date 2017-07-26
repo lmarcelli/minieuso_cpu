4fcb0c07a2478b36f5f49309fd201d5f0d761440
Francesca Capel <capel.francesca@gmail.com>
2017-07-26 13:27:55 +0200

    Added USB identification

--- CPU/CPUsoftware/src/usb_interface.cpp
+++ CPU/CPUsoftware/src/usb_interface.cpp
@@ -1,7 +1,7 @@
 #include "globals.h"
 
 /* print a description of usb devices connected */
-void printdev(libusb_device *dev) {
+void printdev(libusb_device * dev) {
 
   /* get the device descriptor */
   libusb_device_descriptor desc;
@@ -17,15 +17,15 @@ void printdev(libusb_device *dev) {
   std::cout << "ProductID: " << desc.idProduct << std::endl;
 
   /* get the config descriptor */
-  libusb_config_descriptor *config;
+  libusb_config_descriptor * config;
   libusb_get_config_descriptor(dev, 0, &config);
 
   /* print the number of interfaces */
   std::cout << "Interfaces: " << (int)config->bNumInterfaces << " ||| ";
 
-  const libusb_interface *inter;
-  const libusb_interface_descriptor *interdesc;
-  const libusb_endpoint_descriptor *epdesc;
+  const libusb_interface * inter;
+  const libusb_interface_descriptor * interdesc;
+  const libusb_endpoint_descriptor * epdesc;
 
   /* print information on each device */
   for(int i=0; i < (int)config->bNumInterfaces; i++) {
@@ -51,11 +51,12 @@ void printdev(libusb_device *dev) {
   libusb_free_config_descriptor(config);
 }
 
+/* check the number of devices connected and print their info */
 int check_usb() {
   libusb_device ** devs;
   libusb_context * ctx = NULL;
   int r;
-  size_t cnt, i;
+  ssize_t cnt, i;
 
   /* initialise a libusb session */
   r = libusb_init(&ctx);
@@ -86,4 +87,49 @@ int check_usb() {
   libusb_exit(ctx);
   
   return 0;
+}
+
+/* lookup usb devices connected and their ID */
+int lookup_usb() {
+  libusb_device ** all_devs;
+  libusb_device * found = NULL;
+  liusb_device * dev;
+  libusb_context * ctx = NULL;
+  int r;
+  ssize_t cnt, i;
+
+  /* set up logging */
+  std::ofstream log_file(log_name, std::ios::app);
+  logstream clog(log_file, logstream::all);
+  clog << "info: " << logstream::info << "looking up USB devices" << std::endl;
+  
+  /* initialise a libusb session */
+  r = libusb_init(&ctx);
+  if (r < 0) {
+    std::cout << "init error for libusb" << std::endl;
+    return 1;
+  }
+
+  /* set the verbosity level to 3, as suggested by docs */
+  libusb_set_debug(ctx, 3);
+
+  /* get the list of devices */
+  cnt = libusb_get_device_list(ctx, &all_devs);
+  if (cnt < 0) {
+    std::cout << "get device error for libusb" << std::endl;
+    clog << "error: " << logstream::error << "get device error for libusb" << std::endl;
+  }
+
+  /* identify the devices */
+  for (i = 0; i < cnt; i++) {
+    dev = all_devs[i];
+    std::cout << "dev address: " << libusb_get_device_address(dev) << std::endl;
+    std::cout << "port no: " << libusb_get_port_number(dev) << std::endl;
+  }
+  
+  /* clean up */
+  libusb_free_device_list(all_devs, 1);
+  libusb_exit(ctx);
+  
+  return 0;  
 }

f68c0b10c51bce4ca74dee7547fde6a66a9f4dea
Francesca Capel <capel.francesca@gmail.com>
2017-10-02 18:51:14 +0200

    Update Scurve command in ZynqManager

--- CPU/CPUsoftware/src/zynq_interface.cpp
+++ CPU/CPUsoftware/src/zynq_interface.cpp
@@ -297,7 +297,7 @@ int ZynqManager::Scurve(int start, int step, int stop, int acc) {
   
   /* send and receive commands */
   /* take an s-curve */
-  conv << "acq sweep " << start << " " << step << " " << stop << " " << acc << std::endl;
+  conv << "acq scurve " << start << " " << step << " " << stop << " " << acc << std::endl;
   cmd = conv.str();
   std::cout << cmd;
   

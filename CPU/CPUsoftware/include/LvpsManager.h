2e652ae2ab50fde4fd165af6eb988c5be9b8efea
Francesca Capel <capel.francesca@gmail.com>
2017-10-05 17:28:02 +0200

    Add the LvpsManager to the main mecontral software
    This class will control switching and monitoring of subsystems via the LVPS

---
+++ CPU/CPUsoftware/include/LvpsManager.h
@@ -0,0 +1,59 @@
+#ifndef _LVPS_MANAGER_H
+#define _LVPS_MANAGER_H
+
+#include <errno.h>
+#include <error.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+
+#include "aDIO_library.h"
+#include "log.h"
+
+#define ONE_MILLISEC 1000
+
+#define CAMERA_PORT_ON 0x01
+#define CAMERA_PORT_OFF 0x02
+#define HK_PORT_ON 0x04
+#define HK_PORT_OFF 0x08
+#define CC_LVPS_HK 0x10
+#define RET_CC_LVPS_HK 0x20
+
+#define HIGH_VAL 0xFF
+#define LOW_VAL 0x00
+
+class LvpsManager {
+public:
+  enum Status : uint8_t {
+    OFF = 0,
+    ON = 1,
+    UNDEF = 2,
+  };
+  enum SubSystem : uint8_t {
+    CAMERAS = 0,
+    HK = 1,    
+  };
+
+  Status cam_status;
+  Status hk_status;
+  
+  Status GetStatus(SubSystem sub_system);
+  int SwitchOn(SubSystem sub_system);
+  int SwitchOff(SubSystem sub_system);
+  static int Check(SubSystem sub_system);
+
+private:
+  static uint32_t minor_number = 0;
+  enum PortValue : uint8_t {
+    HIGH = 0xFF,
+    LOW = 0x00,
+  };
+
+  int SetDirP0(uint8_t port_config);
+  int SetValP0(PortValue port_value);
+  int SetPulseP0(uint8_t port_config);
+
+};
+
+
+#endif /* LVPS_MANAGER */
---
+++ CPU/CPUsoftware/src/LvpsManager.cpp
@@ -0,0 +1,142 @@
+#include "LvpsManager.h"
+
+/* default constructor */
+LvpsManager::LvpsManager() {
+  this->cam_status = UNDEF;
+  this->hk_status = UNDEF;
+}
+
+/* get the status of a subsystem */
+Status LvpsManager::GetStatus(SubSystem sub_system) {
+  Status sub_system_status = UNDEF;
+
+  switch (sub_system) {
+  case CAMERAS:
+    sub_system_status = this->cam_status;
+    break;
+  case HK:
+    sub_system_status = this->HK_status;
+    break;   
+  }
+
+  return sub_system_status;
+}
+
+/* switch on a subsystem */
+int LvpsManager::SwitchOn(SubSystem sub_system) {
+  Status sub_system_status = UNDEF;
+
+  switch (sub_system) {
+  case CAMERAS:
+ 
+    break;
+  case HK:
+    break;
+
+  }
+  return 0;
+}
+
+/* initialise the aDIO ports */
+int LvpsManager::InitPorts() {
+  int aDIO_ReturnVal;
+ 
+  aDIO_ReturnVal = OpenDIO_aDIO(&aDIO_Device, minor_number);
+  if (aDIO_ReturnVal) {
+    error(EXIT_FAILURE, errno,
+	  "ERROR:  OpenDIO_aDIO(%u) FAILED: MinorNumber(= %u) maybe incorrect",
+	  minor_number, minor_number);
+    return 1;
+  }
+  
+  return 0;
+}
+/* write the direction of P0 */
+int LvpdManager::SetDirP0(uint8_t port_config) {
+  int aDIO_ReturnVal;
+  uint8_t P0Bits[8];
+  int Bit = 0;
+
+  /* write the direction of port 0 */
+  for (Bit = 0; Bit < 8; Bit++) {
+    P0Bits[Bit] = (port_config >> Bit) & 0x01;
+  }
+  
+  /* set the bits of port 0 */
+  aDIO_ReturnVal =
+    LoadPort0BitDir_aDIO(aDIO_Device, P0Bits[7],
+			 P0Bits[6], P0Bits[5],
+			 P0Bits[4], P0Bits[3],
+			 P0Bits[2], P0Bits[1],
+			 P0Bits[0]);
+  /* sleep 1 ms */
+  usleep(ONE_MILLISEC);
+  
+  /* check the return value */
+  if (aDIO_ReturnVal) {
+    error(EXIT_FAILURE, errno,
+	  "ERROR:  LoadPort0bitDir_aDIO() FAILED");
+    return 1;
+  }
+  
+  return 0;
+}
+
+int LvpsManager::SetValP0(PortValue port_value) {
+  int aDIO_ReturnVal;
+  
+  switch (port_value) {
+  case HIGH:
+    aDIO_ReturnVal =
+      WritePort_aDIO(aDIO_Device, 0, HIGH);
+    break;
+  case LOW:
+    aDIO_ReturnVal =
+      WritePort_aDIO(aDIO_Device, 0, LOW);
+    break;
+  }
+ 
+  usleep(ONE_MILLISEC); 
+
+  /* check the return */
+  if (aDIO_ReturnVal) {
+    error(EXIT_FAILURE, errno,
+	  "ERROR:  WritePort_aDIO() FAILED");
+    return 1;
+  }
+
+  return 0;
+}
+/* deliver a 5V, 10 ms pulse to a certain pin of P0 */
+int LvpsManager::SetPulseP0(uint8_t port_config) {
+  int exec_ret = 0;
+
+  /* initialise */
+  exec_ret = InitPorts();
+  if (exec_ret != 0) {
+    clog << "error: " << logstream::error << "could not initialise CPU aDIO ports" << std::endl;
+  }
+  
+  /* set specified output */
+  SetDirP0(port_config);
+  if (exec_ret != 0) {
+    clog << "error: " << logstream::error << "could not set direction of CPU aDIO ports to " << port_config << std::endl;
+  }
+  
+  /* write high */
+  exec_ret = SetValP0(HIGH);
+  if (exec_ret != 0) {
+    clog << "error: " << logstream::error << "could not set value of CPU aDIO P0 " << HIGH << std::endl;
+  }
+  
+  /* sleep for 9 ms */
+  usleep(9 * ONE_MILLISEC);
+
+  /* write low */
+  exec_ret = SetValP0(LOW);
+  if (exec_ret != 0) {
+    clog << "error: " << logstream::error << "could not set value of CPU aDIO P0 " << HIGH << std::endl;
+  }
+  
+  return 0;
+}

64893c6c03f4f8ddd601c6cf1f6cfaa91c5e0dc4
Francesca Capel <capel.francesca@gmail.com>
2017-12-11 10:42:35 +0100

    Increase ID_TAG spacer size in data_format.h

--- CPU/CPUsoftware/include/data_format.h
+++ CPU/CPUsoftware/include/data_format.h
@@ -15,35 +15,35 @@
 
 /* instrument definitions */
 #define INSTRUMENT_ME_PDM 1 /* Instrument Mini-EUSO PDM */
-#define ID_TAG 0xAA55
+#define ID_TAG 0xAA55AA55
 #define RUN_SIZE 25
 
 #pragma pack(push, 1) /* force no padding in structs */
 
 /* cpu file header */
-/* 10 bytes */
+/* 12 bytes */
 typedef struct
 {
-  uint16_t spacer = ID_TAG; /* AA55 HEX */
+  uint32_t spacer = ID_TAG; /* AA55AA55 HEX */
   uint32_t header; /* 'C'(31:24) | instrument_id(23:16) | file_type(15:8) | file_ver(7:0) */
   uint32_t run_size; /* number of cpu packets in the run */
 } CpuFileHeader; 
 
 /* cpu file trailer */
-/* 10 bytes */
+/* 12 bytes */
 typedef struct
 {
-  uint16_t spacer = ID_TAG; /* AA55 HEX */
+  uint32_t spacer = ID_TAG; /* AA55AA55 HEX */
   uint32_t run_size; /* number of cpu packets in the run */
   uint32_t crc; /* checksum */
 } CpuFileTrailer; 
 
 /* generic packet header for all cpu packets and hk/scurve sub packets */
 /* the zynq packet has its own header defined in pdmdata.h */
-/* 14 bytes */
+/* 16 bytes */
 typedef struct
 {
-  uint16_t spacer = ID_TAG; /* AA55 HEX */
+  uint32_t spacer = ID_TAG; /* AA55AA55 HEX */
   uint32_t header; /* 'P'(31:24) | instrument_id(23:16) | pkt_type(15:8) | pkt_ver(7:0) */
   uint32_t pkt_size; /* size of packet */
   uint32_t pkt_num; /* counter for each pkt_type, reset each run */
@@ -82,20 +82,20 @@ typedef struct
 
 
 /* thermistor packet for temperature data */
-/* 58 bytes */
+/* 60 bytes */
 typedef struct
 {
-  CpuPktHeader therm_packet_header; /* 14 bytes */
+  CpuPktHeader therm_packet_header; /* 16 bytes */
   CpuTimeStamp therm_time; /* 4 bytes */
   float therm_data[N_CHANNELS_THERM]; /* 40 bytes */
 } THERM_PACKET;
 
 
 /* housekeeping packet for other data */
-/* 294 bytes */
+/* 296 bytes */
 typedef struct
 {
-  CpuPktHeader hk_packet_header; /* 14 bytes */
+  CpuPktHeader hk_packet_header; /* 16 bytes */
   CpuTimeStamp hk_time; /* 4 bytes */
   float photodiode_data[N_CHANNELS_PHOTODIODE]; /* 16 bytes */ 
   float sipm_data[N_CHANNELS_SIPM]; /* 256 bytes */
@@ -115,10 +115,10 @@ typedef struct
 } ZYNQ_PACKET;
 
 /* CPU packet for incoming data every 5.24 s */
-/* 4719148 bytes */
+/* variable size */
 typedef struct
 {
-  CpuPktHeader cpu_packet_header; /* 14 bytes */
+  CpuPktHeader cpu_packet_header; /* 16 bytes */
   CpuTimeStamp cpu_time; /* 4 bytes */
   ZYNQ_PACKET zynq_packet; /* variable size */
   HK_PACKET hk_packet; /* 294 bytes */
@@ -126,35 +126,35 @@ typedef struct
 
 /* CPU file to store one run */
 /* shown here as demonstration only */
-/* vraiable size */
+/* variable size */
 typedef struct
 {
-  CpuFileHeader cpu_file_header; /* 10 bytes */
+  CpuFileHeader cpu_file_header; /* 12 bytes */
   CPU_PACKET cpu_run_payload[RUN_SIZE]; /* variable size */
-  CpuFileTrailer cpu_file_trailer; /* 10 bytes */
+  CpuFileTrailer cpu_file_trailer; /* 12 bytes */
 } CPU_FILE;
 
 /* SC packet to store S-curve from Zynq  */
-/* 9437218 */
+/* 9437220 */
 typedef struct
 {
-  CpuPktHeader sc_packet_header; /* 14 bytes */
+  CpuPktHeader sc_packet_header; /* 16 bytes */
   CpuTimeStamp sc_time; /* 4 bytes */
-  uint16_t sc_start;
-  uint16_t sc_step;
-  uint16_t sc_stop;
-  uint16_t sc_acc;
+  uint16_t sc_start; /* 2 bytes */
+  uint16_t sc_step; /* 2 bytes */
+  uint16_t sc_stop; /* 2 bytes */
+  uint16_t sc_acc; /* 2 bytes */
   Z_DATA_TYPE_SCURVE_V1 sc_data; /* 9437192 bytes */
 } SC_PACKET;
 
 /* SC file to store a single S-curve */
 /* shown here as demonstration only */
-/* 9437238 bytes (~9 MB) */
+/* 9437244 bytes (~9 MB) */
 typedef struct
 {
-  CpuFileHeader cpu_file_header; /* 10 bytes */
-  SC_PACKET scurve_packet; /* 9437218 bytes */
-  CpuFileTrailer cpu_file_trailer; /* 10 bytes */
+  CpuFileHeader cpu_file_header; /* 12 bytes */
+  SC_PACKET scurve_packet; /* 9437220 bytes */
+  CpuFileTrailer cpu_file_trailer; /* 12 bytes */
 } SC_FILE;
 
 #pragma pack(pop) /* return to normal packing */

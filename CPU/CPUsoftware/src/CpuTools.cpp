fefa9ec4a72121ee36759ff588377ae56a9d9a9c
Francesca Capel <capel.francesca@gmail.com>
2018-01-21 19:11:28 +0100

    Add sleep to CpuTools::SignalHadler to give the program time to read out the HV packet provided on exit

--- CPU/CPUsoftware/src/CpuTools.cpp
+++ CPU/CPUsoftware/src/CpuTools.cpp
@@ -53,6 +53,10 @@ void CpuTools::SignalHandler(int signum) {
   /* stop the data acquisition */
   ZynqManager::StopAcquisition();
   std::cout << "Acquisition stopped" << std::endl;  
+
+  
+  /* wait for the HV file to be read out */
+  sleep(2);
   
   /* turn off the HV */
   //ZynqManager::HvpsTurnOff();

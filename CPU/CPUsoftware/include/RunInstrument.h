8490c435ab865e37ac196a308b489f35b3e42a48
Francesca Capel <capel.francesca@gmail.com>
2018-02-01 17:51:34 +0100

    Update the date and version

--- CPU/CPUsoftware/include/RunInstrument.h
+++ CPU/CPUsoftware/include/RunInstrument.h
@@ -16,8 +16,8 @@
 #define DATA_DIR "/home/minieusouser/DATA"
 
 /* software version and date */
-#define VERSION 4.5
-#define VERSION_DATE_STRING "28/01/2018"
+#define VERSION 4.6
+#define VERSION_DATE_STRING "01/02/2018"
 
 
 /* class to handle different instrument operational modes */

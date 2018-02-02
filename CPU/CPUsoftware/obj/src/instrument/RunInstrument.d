e3a4f80e6c02d5a6b725e075ebc2eddd6935c93d
Francesca Capel <capel.francesca@gmail.com>
2018-02-02 13:38:15 +0100

    UPdate Makefile

--- CPU/CPUsoftware/obj/src/instrument/RunInstrument.d
+++ CPU/CPUsoftware/obj/src/instrument/RunInstrument.d
@@ -2,9 +2,9 @@ obj/src/instrument/RunInstrument.o: src/instrument/RunInstrument.cpp \
   src/instrument/RunInstrument.h src/subsystems/LvpsManager.h \
   src/tools/log.h src/subsystems/UsbManager.h src/tools/CpuTools.h \
   src/subsystems/CamManager.h src/operations/DataAcquisition.h \
-  src/operations/OperationMode.h src/subsystems/ThermManager.h \
-  inc/data_format.h inc/pdmdata.h src/tools/SynchronisedFile.h \
-  src/tools/ConfigManager.h src/subsystems/AnalogManager.h \
+  src/operations/OperationMode.h src/subsystems/AnalogManager.h \
+  inc/data_format.h inc/pdmdata.h src/subsystems/ThermManager.h \
+  src/tools/SynchronisedFile.h src/tools/ConfigManager.h \
   src/subsystems/InputParser.h src/subsystems/ZynqManager.h \
   src/operations/DataReduction.h
 
@@ -24,17 +24,17 @@ src/operations/DataAcquisition.h:
 
 src/operations/OperationMode.h:
 
-src/subsystems/ThermManager.h:
+src/subsystems/AnalogManager.h:
 
 inc/data_format.h:
 
 inc/pdmdata.h:
 
+src/subsystems/ThermManager.h:
+
 src/tools/SynchronisedFile.h:
 
 src/tools/ConfigManager.h:
-
-src/subsystems/AnalogManager.h:
 
 src/subsystems/InputParser.h:
 

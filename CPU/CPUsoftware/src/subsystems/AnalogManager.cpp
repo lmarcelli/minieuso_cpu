3165e6cd3e6d57aa6e4d77e6184361669153ce66
Francesca Capel <capel.francesca@gmail.com>
2018-02-02 17:21:14 +0100

    Fix typo

--- CPU/CPUsoftware/src/subsystems/AnalogManager.cpp
+++ CPU/CPUsoftware/src/subsystems/AnalogManager.cpp
@@ -105,7 +105,7 @@ int AnalogManager::AnalogDataCollect() {
 			     (char *)"DM75xx_ADC_FIFO_Read");
 
 	std::cout << "this->analog_acq.use_count()" << this->analog_acq.use_count() << std::endl;
-	std::cout << "this->analog_acq.unique()" << this->analog_acq.unique() std::endl;
+	std::cout << "this->analog_acq.unique()" << this->analog_acq.unique() << std::endl;
 	this->analog_acq->val[i][j] = ((DM75xx_ADC_ANALOG_DATA(data) / 4096.) * 10);
         /* Check the FIFO status each time */
 	dm75xx_status = DM75xx_FIFO_Get_Status(brd, &data);

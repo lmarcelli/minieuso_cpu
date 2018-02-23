0355ec42a4e9501adaee523af3c9225e04d4d9ef
Francesca Capel <capel.francesca@gmail.com>
2018-02-23 18:00:48 +0100

    Move symlink command to script called by Makefile

---
+++ CPU/CPUsoftware/symlink.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+platform=`uname -s`
+
+if [ "$platform" == "Darwin" ]
+then
+    ln -sf $(PWD)/bin/mecontrol /usr/local/bin/mecontrol
+else
+    LinuxOS=`lsb_release -si`
+    if [ "$OS" != "Ubuntu" ]
+    then
+	ln -sf $(PWD)/bin/mecontrol /usr/local/bin/mecontrol
+    fi
+fi
+

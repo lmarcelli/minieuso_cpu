6c0e56dc371e1b07fbd9273df069645d53d3a71e
Francesca Capel <capel.francesca@gmail.com>
2018-05-28 12:16:45 +0200

    Update the version info

--- minieuso_data_format/minieuso_data_format.h
+++ minieuso_data_format/minieuso_data_format.h
@@ -19,8 +19,8 @@
  * software definitions
  */
 
-#define VERSION 5.7
-#define VERSION_DATE_STRING "24/05/2018"
+#define VERSION 5.8
+#define VERSION_DATE_STRING "28/05/2018"
 
 /*
  * instrument definitions 

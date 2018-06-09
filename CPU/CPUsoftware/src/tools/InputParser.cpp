40ba26d22c0011e56368dec092756b91fb4b86c7
Francesca Capel <capel.francesca@gmail.com>
2018-06-09 17:17:52 +0200

    Remove debug output

--- CPU/CPUsoftware/src/tools/InputParser.cpp
+++ CPU/CPUsoftware/src/tools/InputParser.cpp
@@ -535,20 +535,12 @@ int InputParser::CheckInputs() {
   
   /* loop over inputs and check validity */
   for(auto &t : this->tokens) {
-
-    /* debug */
-    std::cout << "token: " << t << std::endl;
       
     /* only check -options */
     if (t.find('-') != std::string::npos) {
-
-      /* debug */
-      std::cout << "option: " << t << std::endl;
       
       bool allowed = std::find(this->allowed_tokens.begin(), this->allowed_tokens.end(), t)
 	!= this->allowed_tokens.end();
-
-      std::cout << "allowed: " << allowed << std::endl;
       
       if (!allowed) {
 	std::cout << "Error: command line option " << t << " is not recognised" << std::endl;

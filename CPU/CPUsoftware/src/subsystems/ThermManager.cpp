4bcf5849a7e81072bc6976fdd5545a8582a09e0c
Francesca Capel <capel.francesca@gmail.com>
2018-06-09 13:43:06 +0200

    Add therm info to log output

--- CPU/CPUsoftware/src/subsystems/ThermManager.cpp
+++ CPU/CPUsoftware/src/subsystems/ThermManager.cpp
@@ -141,7 +141,7 @@ int ThermManager::WriteThermPkt(TemperatureAcq * temperature_result) {
   THERM_PACKET * therm_packet = new THERM_PACKET();
   static unsigned int pkt_counter = 0;
   
-  clog << "info: " << logstream::info << "writing new packet to " << this->RunAccess->path << std::endl;
+  clog << "info: " << logstream::info << "writing new therm packet to " << this->RunAccess->path << std::endl;
   /* create the therm packet header */
   therm_packet->therm_packet_header.header = BuildCpuPktHeader(THERM_PACKET_TYPE, THERM_PACKET_VER);
   therm_packet->therm_packet_header.pkt_size = sizeof(*therm_packet);
